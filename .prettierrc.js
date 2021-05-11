aa05351509e6f5079a41c48ec75a4c213ec89ce8
icmx <32983315+icmx@users.noreply.github.com>
2021-05-11 22:19:21 +1000

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 72,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+};
